e9c5cd79f4d076aa71436c77c34939ea55b8fde5
Prabash Withanachchi <prabashwithanachchi11111@gmail.com>
2025-12-11 00:55:36 +0530

    cors filter class implemented

--- src/main/java/edu/icet/controller/UserController.java
+++ src/main/java/edu/icet/controller/UserController.java
@@ -3,13 +3,11 @@ package edu.icet.controller;
 import edu.icet.model.dto.User;
 import edu.icet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     @Autowired
     private UserService service;
