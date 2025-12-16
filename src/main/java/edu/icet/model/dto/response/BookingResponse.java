b4ccedf080432887a358e47fea4eb204647cf4ae
Prabash Withanachchi <prabashwithanachchi11111@gmail.com>
2025-12-16 23:30:13 +0530

    BookingRequestReq and BookingResponse  implemented.

---
+++ src/main/java/edu/icet/model/dto/response/BookingResponse.java
@@ -0,0 +1,28 @@
+package edu.icet.model.dto.response;
+
+import edu.icet.model.entity.CarStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class BookingResponse {
+    private Long id;
+    private Long customerId;
+    private String customerName;
+    private String customerEmail;
+    private String registrationNumber;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private double totalAmount;
+    private CarStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
