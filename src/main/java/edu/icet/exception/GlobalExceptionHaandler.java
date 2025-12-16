bb4cd79dd132d8a9b8acc3a24c880f3b42418636
Prabash Withanachchi <prabashwithanachchi11111@gmail.com>
2025-12-16 22:05:57 +0530

    Global exception handler class  implemented

---
+++ src/main/java/edu/icet/exception/GlobalExceptionHaandler.java
@@ -0,0 +1,38 @@
+package edu.icet.exception;
+
+import edu.icet.model.dto.response.ApiResponse;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.lang.module.ResolutionException;
+
+public class GlobalExceptionHaandler {
+
+    @ExceptionHandler(ResolutionException.class)
+    public  ResponseEntity <ApiResponse> handleResourceNotFound(ResolutionException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiResponse> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiResponse> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(ApiResponse.error("Invalid email or password"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("An error occurred: " + ex.getMessage()));
+    }
+
+}
